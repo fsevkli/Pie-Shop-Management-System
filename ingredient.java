92bb513280ef916b7cd059a5fedeadef9bcaf71b
Sevkli <67389767+fsevkli@users.noreply.github.com>
2025-10-21 19:33:08 -0400

    Update ingredient.java

--- ingredient.java
+++ ingredient.java
@@ -1,25 +1,29 @@
-
-public abstract class ingredient {
-	
-	   //  private ArrayList<String> name;
-	    protected double quantity;
-	    protected String unit;
-	    protected String name;
-
-	    
-
-	    public double getQuantity() {
-	        return quantity;
-	    }
-
-	    public String getUnit() {
-	        return unit;
-	    }
-
-	    
-	    public String toString() {
-	        return null;
-	    }
-	}
-	
-
+// Ingredient.java
+public abstract class Ingredient {
+    protected String name;
+    protected double quantity;
+    protected String unit;
+    
+    public Ingredient(String name, double quantity, String unit) {
+        this.name = name;
+        this.quantity = quantity;
+        this.unit = unit;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public double getQuantity() {
+        return quantity;
+    }
+    
+    public String getUnit() {
+        return unit;
+    }
+    
+    @Override
+    public String toString() {
+        return name + " (" + quantity + " " + unit + ")";
+    }
+}
