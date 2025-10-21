43b19d27df82a2f50089ec1d9de26e04d432b09d
Sevkli <67389767+fsevkli@users.noreply.github.com>
2025-10-21 19:32:53 -0400

    Update pie.java

--- pie.java
+++ pie.java
@@ -1,51 +1,43 @@
+// Pie.java
 import java.util.ArrayList;
 
-public class pie {
-	
-	protected String category;
-	protected double price;
-	protected ArrayList<ingredient> ingri = new ArrayList<ingredient>();
-	protected String name;
-	
-	public pie(String name, String category, double price, ArrayList<ingredient> ingri) {
-		
-		this.category = category;
-		this.price = price;
-		this.ingri =ingri;
-		this.name = name;
-		
-			
-	}
-	
-	public String getName() {
-		return name; 
-	}
-	
-	public double getPrice() {
-		
-		return price;
-		
-	}
-	
-	public String getCategory() {
-		return category;
-		
-		
-	}
-	
-	public ArrayList<ingredient> ingri() {
-		return ingri;
-	}
-	
-	public String toString() {
-        return name+':'+ category+','+'$'+price;
-         
-	}
-	
-	
-	
-	
-	
-	
-
+public abstract class Pie {
+    protected String name;
+    protected String category;
+    protected double price;
+    protected ArrayList<Ingredient> ingredients;
+    
+    public Pie(String name, String category, double price, ArrayList<Ingredient> ingredients) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.ingredients = ingredients;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+    
+    public String getCategory() {
+        return category;
+    }
+    
+    public ArrayList<Ingredient> getIngredients() {
+        return ingredients;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(name).append(" (").append(category).append(") - $").append(price);
+        sb.append("\nIngredients: ");
+        for (Ingredient ing : ingredients) {
+            sb.append(ing.toString()).append(", ");
+        }
+        return sb.toString();
+    }
 }
